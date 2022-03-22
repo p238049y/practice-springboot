c55f7e9ead3382887c23f84be4c304eed6bec1c4
p238049y <p238049y@gmail.com>
2022-03-22 23:08:57 +0900

    [update]#2 survey（アンケート）のcontollerにserviceクラスの処理を追加
    [セクション６]３章　DAOパターンを用いたデータベース操作を学習しましょう

--- WebBeginnerStarter/src/main/java/com/example/demo/app/survey/SurveyController.java
+++ WebBeginnerStarter/src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,6 +1,8 @@
 package com.example.demo.app.survey;
 
-import com.example.demo.app.inquiry.InquiryForm;
+import com.example.demo.entity.Survey;
+import com.example.demo.service.SurveyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,14 +14,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Controller
 @RequestMapping("/survey")
 public class SurveyController {
 
-	
+	private final SurveyService surveyService;
+
+	@Autowired
+	public SurveyController(SurveyService surveyService) {
+		this.surveyService = surveyService;
+	}
+
 	@GetMapping
 	public String index(Model model) {
-		model.addAttribute("title", "Survey Form");
+		List<Survey> list = surveyService.getAll();
+
+		model.addAttribute("surveyList", list);
+		model.addAttribute("title", "Survey Index");
+
 		return "survey/index";
 	}
 	
@@ -53,6 +68,15 @@ public class SurveyController {
 			model.addAttribute("title", "Survey Form");
 			return "survey/form";
 		}
+
+		Survey survey = new Survey();
+
+		survey.setAge(surveyForm.getAge());
+		survey.setSatisfaction(surveyForm.getSatisfaction());
+		survey.setComment(surveyForm.getComment());
+		survey.setCreated(LocalDateTime.now());
+
+		surveyService.save(survey);
 		redirectAttributes.addFlashAttribute("complete", "Thank you!");
 		return "redirect:/survey/form";
 	}
