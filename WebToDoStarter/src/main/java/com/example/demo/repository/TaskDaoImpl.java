b06702cdd5a50f71f26525cd4e16be53872fc52a
p238049y <p238049y@gmail.com>
2022-04-16 22:56:31 +0900

    [update] #5  タスクの情報を制御するため処理を追加
    [セクション9] ５章（前編）TODOアプリ開発〜CRUD機能を作りましょう

--- WebToDoStarter/src/main/java/com/example/demo/repository/TaskDaoImpl.java
+++ WebToDoStarter/src/main/java/com/example/demo/repository/TaskDaoImpl.java
@@ -30,10 +30,10 @@ public class TaskDaoImpl implements TaskDao {
         //削除してください
 
         //タスク一覧をMapのListで取得
-        List<Map<String, Object>> resultList = null;
+        List<Map<String, Object>> resultList = jdbcTemplate.queryForList(sql);
 
         //return用の空のListを用意
-        List<Task> list = null;
+        List<Task> list = new ArrayList<Task>();
 
         //二つのテーブルのデータをTaskにまとめる
         for (Map<String, Object> result : resultList) {
@@ -52,6 +52,7 @@ public class TaskDaoImpl implements TaskDao {
             type.setComment((String) result.get("comment"));
 
             //TaskにTaskTypeをセット
+            task.setTaskType(type);
 
             list.add(task);
         }
@@ -66,7 +67,7 @@ public class TaskDaoImpl implements TaskDao {
                 + "WHERE task.id = ?";
 
         //タスクを一件取得
-        Map<String, Object> result = null;
+        Map<String, Object> result = jdbcTemplate.queryForMap(sql, id);
 
         Task task = new Task();
         task.setId((int) result.get("id"));
@@ -82,10 +83,8 @@ public class TaskDaoImpl implements TaskDao {
         type.setComment((String) result.get("comment"));
         task.setTaskType(type);
 
-        //削除してください
-        Optional<Task> taskOpt = null;
-
         //taskをOptionalでラップする
+        Optional<Task> taskOpt = Optional.ofNullable(task);
 
         return taskOpt;
     }
