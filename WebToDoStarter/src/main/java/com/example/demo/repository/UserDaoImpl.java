44d59e41e7a9c2e44d6af303e9410f882f5c6151
p238049y <p238049y@gmail.com>
2022-05-21 20:08:45 +0900

    [fix] #10 userDaoクラスの冗長なコードをまとめる
    [タスク] TaskDaoを元にUserDaoの実装

--- WebToDoStarter/src/main/java/com/example/demo/repository/UserDaoImpl.java
+++ WebToDoStarter/src/main/java/com/example/demo/repository/UserDaoImpl.java
@@ -1,15 +1,47 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.User;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import com.example.demo.entity.User;
 
 @Repository
 public class UserDaoImpl implements UserDao {
+
+    private static final String TABLE_NAME = "user ";
+    private static final String SELECT_SQL = "SELECT id, username, email, password, enabled, authority_id, tempkey ";
+    private static final String FROM_SQL = "FROM " + TABLE_NAME;
+    private static final String PRIMARY_KEY_WHERE_SQL = "WHERE id = ? ";
+    private static final String ORDER_BY_SQL = "ORDER BY id";
+
+    private static final String FIND_ALL_SQL = SELECT_SQL + FROM_SQL + ORDER_BY_SQL;
+    private static final String FIND_ACTIVE_USERS_SQL = SELECT_SQL + FROM_SQL + "WHERE enabled = 1 " + ORDER_BY_SQL;
+    private static final String FIND_BY_ID_SQL = SELECT_SQL + FROM_SQL + PRIMARY_KEY_WHERE_SQL;
+
+    private static final String INSERT_SQL = "INSERT INTO " + TABLE_NAME +
+            "(username, email, password, enabled, authority_id, tempkey) " +
+            "VALUES(?, ?, ?, ?, ?, ?)";
+
+    private static final String UPDATE_SQL = "UPDATE " + TABLE_NAME +
+            "SET username = ?, email = ?, password = ?, enabled = ?, authority_id = ?, tempkey = ? " +
+            PRIMARY_KEY_WHERE_SQL;
+
+    private static final String DELETE_BY_ID_SQL = "DELETE FROM " + TABLE_NAME + PRIMARY_KEY_WHERE_SQL;
+
+    private static final RowMapper<User> USER_ROW_MAPPER = (rs, rowNum) -> {
+        var user = new User();
+        user.setId(rs.getInt("id"));
+        user.setUsername(rs.getString("username"));
+        user.setEmail(rs.getString("email"));
+        user.setPassword(rs.getString("password"));
+        user.setEnabled(rs.getBoolean("enabled"));
+        user.setAuthorityId(rs.getString("authority_id"));
+        user.setTempKey(rs.getString("tempkey"));
+        return user;
+    };
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -18,93 +50,47 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
-    public List<User> findAll(){
-        String sql = "SELECT id, username, email, password, enabled, authority_id, tempKey, "
-                + "FROM"
-                + "user"
-                + "ORDER BY id";
-
-        List<Map<String, Object>> resultList = jdbcTemplate.queryForList(sql);
-
-        List<User> list = new ArrayList<>();
-
-        resultList.forEach(result -> {
-            var user = new User();
-            user.setId((int)result.get("id"));
-            user.setUsername((String) result.get("username"));
-            user.setEmail((String)result.get("email"));
-            user.setPassword((String)result.get("password"));
-            user.setEnabled((boolean) result.get("enabled"));
-            user.setAuthorityId((String) result.get("authority_id"));
-            user.setTempKey((String)result.get("tempKey"));
-
-            list.add(user);
-        });
-
-        return list;
+    public List<User> findAll() {
+        return jdbcTemplate.query(FIND_ALL_SQL, USER_ROW_MAPPER);
     }
 
     @Override
     public List<User> findActiveUsers() {
-        String sql = "SELECT id, username, email, password, enabled, authority_id, tempKey,"
-                + "FROM" + "user"
-                + "WHERE enabled = 1"
-                + "ORDER BY id";
-
-        List<Map<String, Object>> resultList = jdbcTemplate.queryForList(sql);
-
-        List<User> list = new ArrayList<>();
-
-        resultList.forEach(result -> {
-            var user = new User();
-            user.setId((int)result.get("id"));
-            user.setUsername((String) result.get("username"));
-            user.setEmail((String)result.get("email"));
-            user.setPassword((String)result.get("password"));
-            user.setEnabled((boolean) result.get("enabled"));
-            user.setAuthorityId((String) result.get("authority_id"));
-            user.setTempKey((String)result.get("tempKey"));
-
-            list.add(user);
-        });
-
-        return list;
+        return jdbcTemplate.query(FIND_ACTIVE_USERS_SQL, USER_ROW_MAPPER);
     }
 
     @Override
     public User findById(int id) {
-        String sql = "SELECT id, username, email, password, enabled, authority_id, tempKey,"
-                + "FROM" + "user"
-                + "WHERE id = ?";
-
-        Map<String, Object> result = jdbcTemplate.queryForMap(sql, id);
-
-        var user = new User();
-        user.setId((int)result.get("id"));
-        user.setUsername((String) result.get("username"));
-        user.setEmail((String)result.get("email"));
-        user.setPassword((String)result.get("password"));
-        user.setEnabled((boolean) result.get("enabled"));
-        user.setAuthorityId((String) result.get("authority_id"));
-        user.setTempKey((String)result.get("tempKey"));
-
-        return user;
+        return jdbcTemplate.queryForObject(FIND_BY_ID_SQL, USER_ROW_MAPPER, id);
     }
 
     @Override
     public int insert(User user) {
-           return jdbcTemplate.update("INSERT INTO user " + " (username, email, password, enabled, authority_id, tempKey)" + "VALUES(?, ?, ?, ?, ?, ?)",
-                   user.getUsername(), user.getEmail(), user.getPassword(), user.isEnabled(), user.getAuthorityId(), user.getTempKey());
+        return jdbcTemplate.update(
+                INSERT_SQL,
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.getAuthorityId(),
+                user.getTempKey());
     }
 
     @Override
     public int update(User user) {
-        return jdbcTemplate.update("UPDATE user " + "SET username = ?, email = ?, password = ?, enabled = ?, authority_id = ?, tempKey = ? " + "WHERE id = ?",
-                user.getUsername(), user.getEmail(), user.getPassword(), user.isEnabled(), user.getAuthorityId(), user.getTempKey(), user.getId());
+        return jdbcTemplate.update(
+                UPDATE_SQL,
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.getAuthorityId(),
+                user.getTempKey(),
+                user.getId());
     }
 
     @Override
     public int deleteById(int id) {
-        return jdbcTemplate.update("DELETE FROM user WHERE id = ?", id);
+        return jdbcTemplate.update(DELETE_BY_ID_SQL, id);
     }
 }
