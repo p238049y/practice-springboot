a010b7574f216aa43a6bc8037cf1e49481aaa4e3
p238049y <p238049y@gmail.com>
2022-05-23 09:44:50 +0900

    [add] #10 TaskDaoのテストの雛形を作成
    [タスク] TaskDaoを元にUserDaoの実装

---
+++ WebToDoStarter/src/test/java/com/example/demo/repository/TaskDaoImplTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+@SpringBootTest
+@ActiveProfiles("unit")
+@Sql
+class TaskDaoImplTest {
+    @Autowired
+    private TaskDaoImpl taskDao;
+
+//    @Test
+//    @DisplayName("findAllのテスト")
+//    void findAll() {
+//        var list = taskDao.findAll();
+//
+//        // 件数のチェック
+//        Assertions.assertEquals(2, list.size());
+//
+//        // ２件目のレコードの取得
+//        var task2 = list.get(2);
+//        Assertions.assertNotNull(task2);
+//    }
+}
