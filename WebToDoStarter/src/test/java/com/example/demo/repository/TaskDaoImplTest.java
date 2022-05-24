68194437809b8cbc0755c2cbb4633e503b6061ef
p238049y <p238049y@gmail.com>
2022-05-24 21:37:49 +0900

    [add] #10 TaskDaoのfindAll()のテスト
    [タスク] TaskDaoを元にUserDaoの実装

--- WebToDoStarter/src/test/java/com/example/demo/repository/TaskDaoImplTest.java
+++ WebToDoStarter/src/test/java/com/example/demo/repository/TaskDaoImplTest.java
@@ -29,4 +29,19 @@ class TaskDaoImplTest {
 //        var task2 = list.get(2);
 //        Assertions.assertNotNull(task2);
 //    }
+    @Test
+    void findAllのテスト() {
+        var list = taskDao.findAll();
+
+        // 件数のチェック
+        Assertions.assertEquals(2, list.size());
+
+        // ２件目のレコードの取得
+        var task2 = list.get(1);
+        Assertions.assertNotNull(task2);
+
+        // 各カラムの値が正しくセットされているか
+        Assertions.assertEquals("サービスの自作", task2.getTitle());
+        Assertions.assertEquals("マイクロサービスを作ってみる", task2.getDetail());
+    }
 }
